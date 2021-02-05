29737b020af0f3e320d2374b16cef4e48af510b9
Shahzad Qadir <shahzad@shahzadqadir.net>
2021-02-05 15:57:31 +0000

    Separated switch functions from main vlans.py file.

---
+++ switch_functions.py
@@ -0,0 +1,23 @@
+import netmiko
+from getpass import getpass
+
+def pull_switch_vlans(hostname, username, password):
+    """
+    Pull Vlan information and return a vlan-id, name dictionary.
+    """
+    switch1 = {
+        "device_type": "cisco_ios",
+        "host": hostname,
+        "username": username,
+        "password": password       
+    }
+    with netmiko.ConnectHandler(**switch1) as cli_handle:
+        output = cli_handle.send_command("show vlan brief", use_textfsm=True)
+
+    vlans_dict = {}
+    invalid_vlans = ['1002', '1003', '1004', '1005']
+    for item in output:
+        if item['vlan_id'] not in invalid_vlans:
+            vlans_dict[int(item['vlan_id'])] = item['name']
+    
+    return vlans_dict
--- vlans.py
+++ vlans.py
@@ -1,33 +1,9 @@
 #!/home/python/venvs/vlan_sync/bin/python
 
-import netmiko
 from getpass import getpass
 
 from mysql_integration import create_database, pull_db_vlans, add_vlans_db, remove_vlans_db
-
-def pull_switch_vlans(hostname, username, password):
-    """
-    Pull Vlan information and return a vlan-id, name dictionary.
-    """
-    switch1 = {
-        "device_type": "cisco_ios",
-        "host": hostname,
-        "username": username,
-        "password": password       
-    }
-    with netmiko.ConnectHandler(**switch1) as cli_handle:
-        output = cli_handle.send_command("show vlan brief", use_textfsm=True)
-
-    vlans_dict = {}
-    invalid_vlans = ['1002', '1003', '1004', '1005']
-    for item in output:
-        if item['vlan_id'] not in invalid_vlans:
-            vlans_dict[int(item['vlan_id'])] = item['name']
-    
-    return vlans_dict
-
-#############################################
-# Function calls and main logic
+from switch_functions import pull_switch_vlans
 
 db_vlans = pull_db_vlans("localhost", "sqadir", "cisco123", "vlandb")
 sw_hostname = input("Swith IP: ")
