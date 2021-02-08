87743fd8ef9c1b45ec78253b64db4e3ce48c636e
Shahzad Qadir <shahzad@shahzadqadir.net>
2021-02-08 06:26:58 +0000

    dynamic table creation added for vlandb

--- mysql_integration.py
+++ mysql_integration.py
@@ -11,6 +11,8 @@ def create_database(host, username, password):
     )
     db_cursor = mydb.cursor()
     db_cursor.execute("CREATE DATABASE IF NOT EXISTS vlandb")
+    db_cursor.execute("use vlandb")
+    db_cursor.execute("CREATE TABLE IF NOT EXISTS vlans (id INTEGER, name VARCHAR(255))")
 
 def pull_db_vlans(hostname, username, password, db):
     """
